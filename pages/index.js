39de893fb5852584b3dda3d2c22f98c2108f3122
Aral Roca Gomez <aral-rg@hotmail.com>
2020-04-11 23:15:27 +0200

    Add deploy script

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Blog - Aral Roca</title>
+        <title>Aral Roca</title>
       </Head>
       <div>This is the homepage... There is nothing at this moment.</div>
     </>
