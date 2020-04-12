08e11b2516828474bdcff4af9ce58684e76337ad
Aral Roca Gomez <aral-rg@hotmail.com>
2020-04-12 21:49:31 +0200

    Fix mistake

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -16,7 +16,10 @@ export default function Blog({ posts }) {
       {posts.map((post) => {
         return (
           <div className="post-list-item" key={post.slug}>
-            <Link href={`/blog/[slug]?slug=${post.slug}`} as={`/blog/${slug}`}>
+            <Link
+              href={`/blog/[slug]?slug=${post.slug}`}
+              as={`/blog/${post.slug}`}
+            >
               <a>
                 <h2>{post.metadata.title}</h2>
               </a>
