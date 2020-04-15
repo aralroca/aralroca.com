76aa206b16f305f8fa2a433c9219cf4852e4a8b7
Aral Roca Gomez <aral-rg@hotmail.com>
2020-04-16 01:49:41 +0200

    Improve style of change theme selector

--- components/ChangeTheme.js
+++ components/ChangeTheme.js
@@ -1,18 +1,18 @@
 import { useState } from "react"
 
-function Arrows() {
+function Arrows(props) {
   return (
     <svg
       viewBox="0 0 24 24"
-      width="16"
-      height="16"
+      width={16}
+      height={16}
       stroke="currentColor"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
       fill="none"
-      shape-rendering="geometricPrecision"
-      style="color: currentcolor;"
+      shapeRendering="geometricPrecision"
+      {...props}
     >
       <path d="M17 8.517L12 3 7 8.517M7 15.48l5 5.517 5-5.517"></path>
     </svg>
@@ -31,7 +31,6 @@ function Dark() {
       stroke-linejoin="round"
       fill="none"
       shape-rendering="geometricPrecision"
-      style="color: currentcolor;"
     >
       <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"></path>
     </svg>
@@ -50,7 +49,6 @@ function Light() {
       stroke-linejoin="round"
       fill="none"
       shape-rendering="geometricPrecision"
-      style="color: currentcolor;"
     >
       <circle cx="12" cy="12" r="5"></circle>
       <path d="M12 1v2"></path>
@@ -72,12 +70,11 @@ function System() {
       width="16"
       height="16"
       stroke="currentColor"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       fill="none"
-      shape-rendering="geometricPrecision"
-      style="color: currentcolor;"
+      shapeRendering="geometricPrecision"
     >
       <path d="M2 13.381h20M8.66 19.05V22m6.84-2.95V22m-8.955 0h10.932M4 19.05h16a2 2 0 002-2V4a2 2 0 00-2-2H4a2 2 0 00-2 2v13.05a2 2 0 002 2z"></path>
     </svg>
@@ -87,6 +84,11 @@ function System() {
 export default function ChangeTheme() {
   const isNode = typeof window === 'undefined'
   const [theme, setTheme] = useState(isNode ? undefined : window.__theme || 'system')
+  const right = {
+    dark: 30,
+    system: 15,
+    light: 27
+  }[theme]
 
   function onChangeTheme(e) {
     const { value } = e.target
@@ -105,12 +107,12 @@ export default function ChangeTheme() {
       {theme === 'dark' && <Dark />}
       {theme === 'system' && <System />}
       {theme === 'light' && <Light />}
-      <select value={theme} onChange={onChangeTheme}>
+      <select title="Choose another theme" value={theme} onChange={onChangeTheme}>
         <option value="system">System</option>
         <option value="dark">Dark</option>
         <option value="light">Light</option>
       </select>
-      <Arrows />
+      <Arrows style={{ right }} />
     </div>
   )
 }
