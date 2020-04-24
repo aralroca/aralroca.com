73e18a1338b2cd1aa6c11e91007feb66cdecc773
Aral Roca Gomez <aral-rg@hotmail.com>
2020-04-24 02:15:46 +0200

    Fill sections

--- pages/donate.js
+++ pages/donate.js
@@ -7,7 +7,87 @@ export default function Donate() {
         <title key="title">Donate - Aral Roca</title>
       </Head>
       <h1>Donate</h1>
-      This will be the donate page
+      <p>
+        Few years ago I realized that I wanted to be useful and now, with your
+        support, I'll be able to focus on contributing as much as possible to
+        the Open-Source community. Thank you so much ♥️
+      </p>
+      <a
+        href="https://github.com/sponsors/aralroca"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Read more about me and what I do
+      </a>
+
+      <hr className="donate-wrapper-separator" />
+      <div className="donate-wrapper">
+        <div>
+          <h2>Monthly donations</h2>
+          <ul>
+            <li>
+              <a
+                href="https://github.com/sponsors/aralroca"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub Sponsors
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://ko-fi.com/aralroca"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Ko-fi.com
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.patreon.com/aralroca"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Patreon
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h2>One-time donations</h2>
+          <ul>
+            <li>
+              <a
+                href="https://www.buymeacoffee.com/aralroca"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                BuyMeACoffee.com
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://ko-fi.com/aralroca"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Ko-fi.com
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.paypal.me/aralroca"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                PayPal
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   )
 }
--- pages/supporters.js
+++ pages/supporters.js
@@ -1,13 +1,33 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Supporters() {
   return (
-    <>
+    <div className="supporters">
       <Head>
         <title key="title">Supporters - Aral Roca</title>
       </Head>
       <h1>Supporters</h1>
-      This will be the supporters page
-    </>
+      <p>
+        Thank you for making my Open-Source work possible.{' '}
+        <u>You're awesome!</u>
+      </p>
+
+      <h2>🌕 Platinum supporters</h2>
+      <em>None.</em>
+
+      <h2>🌔 Silver supporters</h2>
+      <em>None.</em>
+
+      <h2>🌓 Supporters</h2>
+      <em>None.</em>
+
+      <h2>Become a supporter</h2>
+      <p>
+        <Link href="/donate">
+          <a>Support my open source work </a>
+        </Link>
+      </p>
+    </div>
   )
 }
