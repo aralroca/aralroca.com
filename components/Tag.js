90c08b66576f7b0b2196dfcff563fa3083be35ac
Aral Roca Gomez <aral-rg@hotmail.com>
2020-07-05 23:18:42 +0200

    Implement pagination (#64)
    * Implement pagination
    
    * Remove log
    
    * Temporal fix minify

--- components/Tag.js
+++ components/Tag.js
@@ -2,14 +2,14 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 
 export default function Tag({ label, search = '', onClick }) {
-  const { pathname, asPath } = useRouter()
+  const { pathname, query } = useRouter()
   const tag = label.toLowerCase()
   const tags = search.split(' ').map((t) => t.toLowerCase())
   const isActive = tags.includes(tag)
   let href = `/blog?q=${label}`
 
   if (pathname === '/blog') {
-    href = asPath === pathname ? `/blog?q=${label}` : `${asPath}+${label}`
+    href = query.q ? `${pathname}?q=${query.q}+${label}` : `/blog?q=${label}`
   }
 
   if (isActive) {
