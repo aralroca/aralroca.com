9fb87e8430b3d5e4f9a7f64af5bd1e7faf2ba03d
Aral Roca Gomez <aral-rg@hotmail.com>
2020-09-06 13:32:58 +0200

    Fix newsletter fetch

--- publish/newsletter.js
+++ publish/newsletter.js
@@ -6,15 +6,20 @@ async function newsletter(post) {
   const { sent, response } = await fetch(url, {
     method: 'POST',
     headers: {
-      'api-key': process.env.MANDRILL_API_KEY
+      'api-key': process.env.MANDRILL_API_KEY,
     },
     body: JSON.stringify({
       from: 'contact@aralroca.com',
       to: 'aral-rg@hotmail.com',
-      subject: "This is a test",
-      html: `This email is <b>a test</b>!`
-    })
+      subject: 'This is a test',
+      html: `This email is <b>a test</b>!`,
+    }),
   })
+    .then((r) => r.json())
+    .catch((e) => {
+      console.log('newsletter -> KO', e)
+      return {}
+    })
 
   console.log('newsletter -> @todo', { sent, response })
 }
