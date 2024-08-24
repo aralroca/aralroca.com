b4c541a897e07cb9e24f7a08f1565d43d7f8d7f2
Aral Roca <aral-rg@hotmail.com>
2024-08-24 09:48:30 +0200

    chore: update dependencies

--- src/utils/readPost.tsx
+++ src/utils/readPost.tsx
@@ -1,10 +1,11 @@
 import fs from 'node:fs';
 // @ts-ignore
-import { marked } from 'marked';
+import { Marked, marked } from 'marked';
 import matter from 'gray-matter';
 import path from 'node:path';
 import readingTime from 'reading-time';
 import niceDateText from './niceDateText';
+import { markedHighlight } from 'marked-highlight';
 
 export type PostContent = {
   data: any;
@@ -27,21 +28,24 @@ export default function readPost(slug: string): PostContent {
   const markdownWithMetadata = fs
     .readFileSync(path.join(POST_PATH, slug + '.md'))
     .toString();
+  const hljs = require('highlight.js');
 
-  marked.setOptions({
-    highlight: (code: string, language: string) => {
-      const hljs = require('highlight.js');
-      const validLanguage = hljs.getLanguage(language) ? language : 'plaintext';
-      return hljs.highlight(code, { language: validLanguage }).value;
-    },
-  });
+  const marked = new Marked(
+    markedHighlight({
+      langPrefix: 'hljs language-',
+      highlight(code, lang, info) {
+        const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+        return hljs.highlight(code, { language }).value;
+      },
+    }),
+  );
 
   const { data, content } = matter(markdownWithMetadata);
 
   return {
     data,
     date: niceDateText(new Date(data.created)),
-    __html: marked(content),
+    __html: marked.parse(content).toString(),
     timeToRead: readingTime(content),
   };
 }
