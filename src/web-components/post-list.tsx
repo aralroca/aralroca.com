a28a557a465dc19e1e6e4282b10bd616e03af814
Aral Roca <aral-rg@hotmail.com>
2024-10-02 16:59:50 +0200

    fix

--- src/web-components/post-list.tsx
+++ src/web-components/post-list.tsx
@@ -10,18 +10,18 @@ export default function PostList(
   { store, params, derived }: WebContext,
 ) {
   const filteredPosts = derived(() =>
-    params.value?.q
-      ? store.get<any>('posts').filter(filterSearch(params.value?.q))
+    params?.value?.q
+      ? store.get<any>('posts').filter(filterSearch(params?.value?.q))
       : store.get<any>('posts'),
   );
-  const currentPage = derived(() => Number(params.value?.page || 1));
+  const currentPage = derived(() => Number(params?.value?.page || 1));
   const pages = derived(() =>
     Math.ceil(filteredPosts.value?.length / itemsPerPage),
   );
   const postsToShow = derived(() => {
     const lastIndex = itemsPerPage * currentPage.value;
     const firstIndex = lastIndex - itemsPerPage;
-    return filteredPosts.value?.slice(firstIndex, lastIndex);
+    return filteredPosts?.value?.slice(firstIndex, lastIndex);
   });
 
   return (
@@ -30,10 +30,10 @@ export default function PostList(
         <div class="posts-box">
           <div class="blog-title">
             <slot name="title" />
-            <div>{filteredPosts.value?.length} posts</div>
+            <div>{filteredPosts?.value?.length} posts</div>
           </div>
 
-          {postsToShow.value?.map?.(
+          {postsToShow?.value?.map?.(
             ({ slug, metadata, date, timeToRead }: Post) => (
               <a
                 href={`/blog/${slug}`}
@@ -62,7 +62,7 @@ export default function PostList(
             ),
           )}
 
-          {pages.value > 1 && (
+          {pages?.value > 1 && (
             <div class="paginator">
               {pageBadges({
                 currentPage: currentPage.value,
@@ -71,7 +71,7 @@ export default function PostList(
                 num ? (
                   <a
                     key={`page-${num}`}
-                    href={`/blog?q=${params.value?.q || ''}&page=${num}`}
+                    href={`/blog?q=${params?.value?.q || ''}&page=${num}`}
                     class={`badge ${num === currentPage.value ? 'current' : ''}`}
                   >
                     {num}{' '}
@@ -85,13 +85,13 @@ export default function PostList(
             </div>
           )}
 
-          {filteredPosts.value?.length === 0 && (
+          {filteredPosts?.value?.length === 0 && (
             <div style={{ marginTop: 50, textAlign: 'center' }}>
               Can't find what you're looking for? Try using{' '}
               <a
                 target="_blank"
                 rel="noopener noreferrer"
-                href={`https://www.google.com/search?q=site%3Aaralroca.com+${params.value.q}`}
+                href={`https://www.google.com/search?q=site%3Aaralroca.com+${params?.value.q}`}
               >
                 Google
               </a>
@@ -104,7 +104,7 @@ export default function PostList(
           <div class="sticky">
             <input
               class="post-searcher"
-              value={params.value?.q ?? ''}
+              value={params?.value?.q ?? ''}
               onInput={(e) => {
                 const input = e.target as HTMLInputElement;
                 const q = input.value;
@@ -119,7 +119,7 @@ export default function PostList(
                 Tag({
                   key: tag,
                   label: tag,
-                  q: params.value?.q ?? '',
+                  q: params?.value?.q ?? '',
                 }),
               )}
             </div>
